9fffabef72a8d177b64599253f0fa1da6f80116c
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-05-11 04:24:22 +0100

    refactor/legacy_audio (#10)
    * refactor/legacy_audio
    
    use abstractmethods to force implementation of key pieces in plugins
    
    drop dependency on common_play base class
    
    helps keeping the legacy plugins alive independently of OCP
    
    * ensure ocp status messages
    
    * compat with older dependency versions
    
    just want to drop dependency on OCP in this PR

--- ovos_audio_plugin_simple/__init__.py
+++ ovos_audio_plugin_simple/__init__.py
@@ -2,15 +2,46 @@ import mimetypes
 import re
 import signal
 import subprocess
+import time
 from distutils.spawn import find_executable
 from time import sleep
 
-from ovos_plugin_common_play.ocp.base import OCPAudioPlayerBackend
-from ovos_plugin_common_play.ocp.status import TrackState, MediaState, PlayerState
-from ovos_plugin_manager.templates.audio import AudioBackend
 from ovos_bus_client import Message
+from ovos_plugin_manager.templates.audio import AudioBackend
 from ovos_utils.log import LOG
 from requests import Session
+
+try:
+    from ovos_utils.ocp import MediaState
+except ImportError:
+    LOG.warning("Please update to ovos-utils~=0.1.")
+    from enum import IntEnum
+
+
+    class MediaState(IntEnum):
+        # https://doc.qt.io/qt-5/qmediaplayer.html#MediaStatus-enum
+        # The status of the media cannot be determined.
+        UNKNOWN = 0
+        # There is no current media. PlayerState == STOPPED
+        NO_MEDIA = 1
+        # The current media is being loaded. The player may be in any state.
+        LOADING_MEDIA = 2
+        # The current media has been loaded. PlayerState== STOPPED
+        LOADED_MEDIA = 3
+        # Playback of the current media has stalled due to
+        # insufficient buffering or some other temporary interruption.
+        # PlayerState != STOPPED
+        STALLED_MEDIA = 4
+        # The player is buffering data but has enough data buffered
+        # for playback to continue for the immediate future.
+        # PlayerState != STOPPED
+        BUFFERING_MEDIA = 5
+        # The player has fully buffered the current media. PlayerState != STOPPED
+        BUFFERED_MEDIA = 6
+        # Playback has reached the end of the current media. PlayerState == STOPPED
+        END_OF_MEDIA = 7
+        # The current media cannot be played. PlayerState == STOPPED
+        INVALID_MEDIA = 8
 
 
 def find_mime(path):
@@ -46,15 +77,7 @@ def play_audio(uri, play_cmd="play"):
         return None
 
 
-SimpleAudioPluginConfig = {
-    "simple": {
-        "type": "ovos_simple",
-        "active": True
-    }
-}
-
-
-class OVOSSimpleService(OCPAudioPlayerBackend):
+class OVOSSimpleService(AudioBackend):
     sox_play = find_executable("play")
     pulse_play = find_executable("paplay")
     alsa_play = find_executable("aplay")
@@ -67,6 +90,8 @@ class OVOSSimpleService(OCPAudioPlayerBackend):
         self.process = None
         self._stop_signal = False
         self._is_playing = False
+        self._time_accumulator = 0
+        self._start = 0
         self._paused = False
 
         self.supports_mime_hints = True
@@ -74,6 +99,7 @@ class OVOSSimpleService(OCPAudioPlayerBackend):
 
         self.bus.on('ovos.common_play.simple.play', self._play)
 
+    ###################
     # simple player internals
     def _get_track(self, track_data):
         if isinstance(track_data, list):
@@ -155,30 +181,41 @@ class OVOSSimpleService(OCPAudioPlayerBackend):
         except FileNotFoundError as e:
             LOG.error(f'Couldn\'t play audio, {e}')
             self.process = None
-            self.ocp_error()
         except Exception as e:
             LOG.exception(repr(e))
             self.process = None
-            self.ocp_error()
 
-        # Wait for completion or stop request
-        while (self._is_process_running() and not self._stop_signal):
-            sleep(0.25)
+        if self.process:
+            # Wait for completion or stop request
+            while (self._is_process_running() and not self._stop_signal):
+                sleep(0.25)
 
-        if self._stop_signal:
-            self._stop_running_process()
-            self._is_playing = False
-            self._paused = False
-            return
+            if self._stop_signal:
+                self._stop_running_process()
+                self._is_playing = False
+                self._paused = False
+                return
+            else:
+                self.process = None
         else:
-            self.process = None
+            try:
+                self.ocp_error()
+            except:  # too old OPM version
+                self.bus.emit(Message("ovos.common_play.media.state",
+                                      {"state": MediaState.INVALID_MEDIA}))
 
         self._track_start_callback(None)
         self._is_playing = False
         self._paused = False
-        self.ocp_stop()
 
-    # audio service
+    ############
+    # mandatory abstract methods
+
+    @property
+    def playback_time(self):
+        """ in milliseconds """
+        return max(self._start - time.time(), 0) + self._time_accumulator
+
     def supported_uris(self):
         uris = ['file', 'http']
         if self.sox_play:
@@ -187,41 +224,88 @@ class OVOSSimpleService(OCPAudioPlayerBackend):
 
     def play(self, repeat=False):
         """ Play playlist using simple. """
-        self.ocp_start()
+        self._start = time.time()
+        self._time_accumulator = 0
         self.bus.emit(Message('ovos.common_play.simple.play',
                               {'repeat': repeat}))
 
     def stop(self):
         """ Stop simple playback. """
         LOG.info('SimpleService Stop')
+        self._start = self._time_accumulator = 0
         if self._is_playing:
             self._stop_signal = True
             while self._is_playing:
                 sleep(0.1)
             self._stop_signal = False
-            self.ocp_stop()
             return True
         return False
 
     def pause(self):
         """ Pause simple playback. """
         if self.process and not self._paused:
+            if self._start:
+                self._time_accumulator += time.time() - self._start
+                self._start = 0
             # Suspend the playback process
             self.process.send_signal(signal.SIGSTOP)
             self._paused = True
-            self.ocp_pause()
 
     def resume(self):
         """ Resume paused playback. """
         if self.process and self._paused:
+            self._start = time.time()
             # Resume the playback process
             self.process.send_signal(signal.SIGCONT)
             self._paused = False
-            self.ocp_resume()
 
-    def track_info(self):
-        """ Extract info of current track. """
-        return {"track": self._now_playing}
+    def get_track_position(self):
+        """
+        get current position in milliseconds
+        """
+        return self.playback_time
+
+    ################
+    # missing features in simple audio plugin
+    def lower_volume(self):
+        LOG.error("simple audio player does not support lower_volume")
+
+    def restore_volume(self):
+        LOG.error("simple audio player does not support restore_volume")
+
+    def get_track_length(self):
+        """
+        getting the duration of the audio in milliseconds
+        """
+        LOG.error("simple audio player does not support get_track_length")
+        return self.playback_time
+
+    def set_track_position(self, milliseconds):
+        """
+        go to position in milliseconds
+
+          Args:
+                milliseconds (int): number of milliseconds of final position
+        """
+        LOG.error("simple audio player does not support set_track_position")
+
+    def seek_forward(self, seconds=1):
+        """
+        skip X seconds
+
+          Args:
+                seconds (int): number of seconds to seek, if negative rewind
+        """
+        LOG.error("simple audio player does not support seek_forward")
+
+    def seek_backward(self, seconds=1):
+        """
+        rewind X seconds
+
+          Args:
+                seconds (int): number of seconds to seek, if negative rewind
+        """
+        LOG.error("simple audio player does not support seek_backward")
 
 
 def load_service(base_config, bus):
@@ -239,3 +323,11 @@ def load_service(base_config, bus):
 
     instances = [OVOSSimpleService(s[1], bus, s[0]) for s in services]
     return instances
+
+
+SimpleAudioPluginConfig = {
+    "simple": {
+        "type": "ovos_simple",
+        "active": True
+    }
+}
